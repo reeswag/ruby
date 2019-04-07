59b4c6d8b650c009abfc4863a64d0e9dfc893adf
reeswag <reeswag@gmail.com>
2019-04-07 07:55:58 +0100

    Ex51

--- SS2018_AdvancedRubyCourse/Ruby_Files/projects/gothonweb/bin/app.rb
+++ SS2018_AdvancedRubyCourse/Ruby_Files/projects/gothonweb/bin/app.rb
@@ -5,13 +5,18 @@ set :static, true
 set :public_folder, "static"
 set :views, "views"
 
-get '/' do
+get '/' do # this is the handler - it specifies what happens when you visit a certain page
     return 'Hello world'
 end
 
 get '/hello/' do
-    greeting = params[:greeting] || "Hi There" # the || says either use what is in params or "Hi There"
-    erb :index, :locals => {'greeting' => greeting}
+    erb :hello_form
 end
 
+post '/hello/' do # the post indicates that we will be recieving a form 
+    greeting = params[:greeting] || "Hi There" # Takes the greeting parameter from the form
+    name = params[:name] || "Nobody" # Takes the name parameter from the form
+
+    erb :index, :locals => {'greeting' => greeting, 'name' => name} # Passes the above parameters to the index template.
+end
 
