ebe142b0e20eaf6877f147073817fc1299fe9dad
reeswag <reeswag@gmail.com>
2019-04-19 10:36:17 +0100

    re-structure

---
+++ HTML/HTML_Notes.md
@@ -0,0 +1,17 @@
+# HTML Notes
+## Tables
+### Radio Buttons
+HTML radio buttons are a type of HTML forms used to enable website visitors to select a single option from a list of multiple options. HTML radio buttons create round check buttons next to the text.
+
+They are created using an \<input> element whose type attribute is specified with a value of radio. Every HTML radio button must have a similar name attribute to create a neat looking picture.
+
+Example
+``` 
+<form>
+  <input type="radio" name="day" value="monday" checked>Monday<br>
+  <input type="radio" name="day" value="tuesday">Tuesday<br>
+  <input type="radio" name="day" value="wednesday">Wednesday
+</form>
+```
+
+
--- SS2018_AdvancedRubyCourse/Ruby_Files/projects/voting/bin/voting.rb
+++ SS2018_AdvancedRubyCourse/Ruby_Files/projects/voting/bin/voting.rb
@@ -8,7 +8,7 @@ set :static, true
 set :public_folder, "static"
 set :views, "views"
 
-$choices = {
+$choices = { # all possible voting choices are assigned here
     'HAM' => 'Hamburger',
     'PIZ' => 'Pizza',
     'CUR' => 'Curry',
@@ -22,13 +22,12 @@ end
 
 post '/cast' do 
     @title = "Thanks for casting your vote!"
-    @vote = params['vote']
-    @store = YAML::Store.new "votes.yml"
-    puts File.dirname('@store') 
-    @store.transaction do
-        @store['votes'] ||= {}
-        @store['votes'][@vote] ||=0
-        @store['votes'][@vote] += 1
+    @vote = params['vote'] # assignes the instance variable to which ever Key was selected from the choices hash
+    @store = YAML::Store.new "votes.yml" # creates a temp yaml store file instance
+    @store.transaction do # accesses the votes.yml file (creates it if it doesn't already exist) and runs the block below 
+        @store['votes'] ||= {} # the ||= stops the variable from being overwritten if it already exists. 
+        @store['votes'][@vote] ||= 0 # the vote choice is assigned a count of 0 unless it already exists
+        @store['votes'][@vote] += 1 # 1 vote is added to the total 
     end
     erb :cast, :locals => {'title' => @title, 'vote' => @vote}
 end
@@ -36,6 +35,6 @@ end
 get '/results' do
     @title = 'Results so far:'
     @store = YAML::Store.new 'votes.yml'
-    @votes = @store.transaction {@store['votes']}
+    @votes = @store.transaction {@store['votes']} # accesses the votes hash
     erb :results, :locals => {'title' => @title} 
 end
